6e992264496e1a86058ad843f05124ba2eb74045
Ben Ford <ben.ford@puppetlabs.com>
2016-05-11 13:04:42 -0700

    reparented website content

--- shell/puppet_module_setup.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest https://forgeapi.puppetlabs.com | out-null
-puppet module install puppetlabs-dsc
-puppet module install chocolatey-chocolatey
-puppet module install puppetlabs-registry
--- shell/vagrant_setup.ps1
+++
@@ -1,7 +0,0 @@
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install puppet-agent -y
-
-$p = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::Machine)
-if(-not($p.EndsWith(";")){
-  [System.Environment]::SetEnvironmentVariable("PATH", ($p + ";"), [System.EnvironmentVariableTarget]::Machine)
-}
